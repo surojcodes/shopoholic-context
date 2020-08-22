56bbae9802a657c5bfe1d61568dbee1450c796ef
Suroj Maharjan <surojcodes@gmail.com>
2020-08-22 17:02:39 +0545

    UI completed

---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <>
+      <div className='' style={{ margin: '8em auto' }}>
+        <div className='container text-center'>
+          <p className='display-4'>
+            404 <br /> PAGE NOT FOUND
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
